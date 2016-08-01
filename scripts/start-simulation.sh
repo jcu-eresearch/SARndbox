0cf2e3c241aab0fcfd880f1e6df530ca9d0d359b
David Beitey <david@davidjb.com>
2016-08-01 14:13:52 +1000

    Add colours and configuration

---
+++ scripts/start-simulation.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+~/src/SARndbox-1.6/bin/SARndbox -uhm -ws 0.0 0 -cl 1.5
---
+++ scripts/start-water.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Standard rain strength and evaporation
+# Tends to be hard to get enough rain to stay on the field
+#~/src/SARndbox-1.6/bin/SARndbox -uhm -ws 1.0 30 -cl 1.5 -evr -.05
+
+# With stronger rain and evaporation
+~/src/SARndbox-1.6/bin/SARndbox -uhm -ws 1.0 30 -cl 1.5 -evr -.05 -rs 4
+
